621ec5454b9f60b25ce0e54c1b4c782d189cc117
philbillbell <philbillbell@hotmail.com>
2016-02-10 13:02:38 +0000

    unit test added for game rules

---
+++ Connect4_Test/rules_test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game_Rules;
+
+namespace Connect4_Test
+{
+    [TestClass]
+    public class rules_test
+    {
+
+        private String[,] board;
+        private int rows;
+        private int cols;
+
+        [TestMethod]
+        public void check_for_horizontal_win()
+        {
+            rows = 6;
+            cols = 7;
+
+            board = new String[rows, cols];
+
+            GameRules rules = new GameRules();
+            Assert.AreEqual(rules.check_for_win(board), true);
+
+
+        }
+    }
+}
---
+++ Game_Rules/GameRules.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Rules
+{
+    public class GameRules
+    {
+        public GameRules()
+        { }
+
+
+
+        public bool check_for_win(String[,] board)
+        {
+            return false;
+
+        }
+    }
+}
